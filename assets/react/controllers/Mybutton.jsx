00c98326711905a78edd2db8b6cfa7e0647ccee3
chloe <michelchloe04@gmail.com>
2023-12-14 14:36:01 +0100

    oui

---
+++ assets/react/controllers/Mybutton.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+export default function Bouton() {
+    return (
+        <Button variant="primary">Ciquez-moi</Button>
+    )
+}
