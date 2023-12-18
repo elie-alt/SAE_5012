f9730093d29a193a688e4a7f82aff021824de719
chloe <michelchloe04@gmail.com>
2023-12-18 15:39:32 +0100

    login ok

--- assets/react/controllers/MyLoginForm.jsx
+++ assets/react/controllers/MyLoginForm.jsx
@@ -1,74 +1,31 @@
-import React, { useState } from 'react';
-import { Container, Row, Col, Card, Form, Button } from 'react-bootstrap';
+import React from 'react';
+import '../../styles/LoginFormStyles.css';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
-const MyLoginForm = () => {
-  const [isLogin, setIsLogin] = useState(true);
-
-  const handleToggle = () => {
-    setIsLogin(!isLogin);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Add your form submission logic here
-  };
-
+function BasicExample() {
   return (
-    <Container>
-      <Row className="justify-content-center">
-        <Col md={6}>
-          <Card className="mt-5">
-            <Card.Body>
-              <h6 className="mb-4">
-                <span onClick={handleToggle} style={{ cursor: 'pointer' }}>
-                  Log In
-                </span>
-                <span onClick={handleToggle} style={{ cursor: 'pointer' }}>
-                  Sign Up
-                </span>
-              </h6>
-              <Form onSubmit={handleSubmit}>
-                {/* Common Form Fields */}
-                {isLogin ? (
-                  <>
-                    {/* Login Form Fields */}
-                    <Form.Group controlId="formBasicEmail">
-                      <Form.Control type="email" placeholder="Your Email" />
-                    </Form.Group>
-                    <Form.Group controlId="formBasicPassword">
-                      <Form.Control type="password" placeholder="Your Password" />
-                    </Form.Group>
-                  </>
-                ) : (
-                  <>
-                    {/* Sign Up Form Fields */}
-                    <Form.Group controlId="formBasicName">
-                      <Form.Control type="text" placeholder="Your Full Name" />
-                    </Form.Group>
-                    <Form.Group controlId="formBasicEmail">
-                      <Form.Control type="email" placeholder="Your Email" />
-                    </Form.Group>
-                    <Form.Group controlId="formBasicPassword">
-                      <Form.Control type="password" placeholder="Your Password" />
-                    </Form.Group>
-                  </>
-                )}
+    <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
 
-                <Button variant="primary" type="submit" className="mt-3">
-                  Submit
-                </Button>
-              </Form>
-              <p className="mt-4 text-center">
-                <a href="#0" className="link">
-                  Forgot your password?
-                </a>
-              </p>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
   );
-};
+}
 
-export default MyLoginForm;
+export default BasicExample;
