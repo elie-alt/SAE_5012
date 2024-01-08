a9f00d8de97e73ea047c1eabbcd9b606ab734f2d
chloe <michelchloe04@gmail.com>
2024-01-08 14:29:11 +0100

    feat: articles, footer, nav

--- assets/react/controllers/Homepage.jsx
+++ assets/react/controllers/Homepage.jsx
@@ -4,26 +4,7 @@ import { Navbar, Nav, Container, Card, Badge } from 'react-bootstrap';
 function App(props) {
   return (
     <div className="d-flex flex-column">
-      <main className="flex-shrink-0">
-        {/* Navigation */}
-        <Navbar bg="dark" expand="lg" variant="dark">
-          <Container>
-            <Navbar.Brand  href={props.LinkBrand}>CMS</Navbar.Brand>
-            <Navbar.Toggle aria-controls="navbarSupportedContent" />
-            <Navbar.Collapse id="navbarSupportedContent">
-              <Nav className="ms-auto mb-2 mb-lg-0">
-                <Nav.Link  href={props.LinkHome}>Accueil</Nav.Link>
-                <Nav.Link href={props.LinkArticleShow}>Les articles</Nav.Link>
-                <Nav.Link href={props.LinkBrand}>Contact</Nav.Link>
-                <Nav.Link href="#pricing">Pricing</Nav.Link>
-                <Nav.Link href="#faq">FAQ</Nav.Link>
-                <Nav.Link href="#blog">Blog</Nav.Link>
-                <Nav.Link href="#portfolio">Portfolio</Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-        
+      <main className="flex-shrink-0">        
         {/* Page Content */}
         <section className="py-5">
           <Container>
@@ -50,25 +31,7 @@ function App(props) {
             </Card>
           </Container>
         </section>
-
-        {/* Add other sections here using React-Bootstrap components */}
       </main>
-
-      {/* Footer */}
-      <footer className="bg-dark py-4 mt-auto">
-        <Container>
-          <div className="row align-items-center justify-content-between flex-column flex-sm-row">
-            <div className="col-auto"><div className="small m-0 text-white">Copyright &copy; Your Website 2023</div></div>
-            <div className="col-auto">
-              <a className="link-light small" href="#!">Privacy</a>
-              <span className="text-white mx-1">&middot;</span>
-              <a className="link-light small" href="#!">Terms</a>
-              <span className="text-white mx-1">&middot;</span>
-              <a className="link-light small" href="#!">Contact</a>
-            </div>
-          </div>
-        </Container>
-      </footer>
     </div>
   );
 }
