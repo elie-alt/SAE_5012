38e4446366da4a978d3b864b1f647425b7ede76d
elie-alt <elie.archambault3@gmail.com>
2024-01-09 13:41:33 +0100

    h

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,7 +6,9 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -18,39 +20,30 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-    ->add('email', EmailType::class, [
-        'label' => false,
-        'attr' => [
-            'placeholder' => 'e-mail',
-            'class' => 'form-control mb-3',
-        ],
-    ])
-            // ->add('agreeTerms', CheckboxType::class, [
-            //     'mapped' => false,
-            //     'constraints' => [
-            //         new IsTrue([
-            //             'message' => 'Vous devez accepter les termes.',
-            //         ]),
-            //     ],
-            // ])
-    ->add('plainPassword', PasswordType::class, [
-        'label' => false,
-        'mapped' => false,
-        'attr' => [
-            'autocomplete' => 'new-password',
-            'placeholder' => 'mot de passe',
-            'class' => 'form-control mb-3',
-        ],
-    
-
+            ->add('username', TextType::class,[
+                'label' => "Nom d'utilisateur"
+            ])
+            ->add('plainPassword', RepeatedType::class, [
+                'mapped' => false,
+                'type' => PasswordType::class,
+                // 'attr' => [
+                //     'autocomplete' => 'new-password',
+                //     'placeholder' => 'mot de passe',
+                //     'class' => 'form-control mb-3',
+                // ],
+                // 'options' => [
+                //     'attr' => [
+                //         'type' => 'password'
+                //     ]
+                // ],
+                'first_options' => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Confirmer le mot de passe'],
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Entrez un mot de passe',
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Votre mot de passe doit faire au moins {{ limit }} characteres',
-                        // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
                 ],
