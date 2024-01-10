6ce77a1c94902c05befb357b0167018826d0b8ac
elie-alt <elie.archambault3@gmail.com>
2024-01-10 15:32:58 +0100

    user

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -14,7 +14,7 @@ class SecurityController extends AbstractController
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         if ($this->getUser()) {
-            return $this->redirectToRoute('target_path');
+            return $this->redirectToRoute('app_home');
         }
 
         // get the login error if there is one
