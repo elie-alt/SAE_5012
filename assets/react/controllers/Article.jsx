bb8bb994c499d738f29fb58006a5f64d2a868955
chloe <michelchloe04@gmail.com>
2024-01-16 08:29:48 +0100

    y'a de nouveau le front

--- assets/react/controllers/Article.jsx
+++ assets/react/controllers/Article.jsx
@@ -1,34 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 
-import { Container, Row } from 'react-bootstrap';
+// import { Container, Row } from 'react-bootstrap';
 
-const reactContainer = document.getElementById('react-article-container');
-const dynamicData = JSON.parse(reactContainer.dataset.dynamicData);
+// const reactContainer = document.getElementById('react-article-container');
+// const dynamicData = JSON.parse(reactContainer.dataset.dynamicData);
 
 
-ReactDOM.render(<Article article={dynamicData} />, reactContainer);
+// ReactDOM.render(<Article article={dynamicData} />, reactContainer);
 
-export default function Article(props) {
-    const { ArticleTitle, ArticleContent, ArticleComment } = props.article; // Correction ici
+// export default function Article(props) {
+//     const { ArticleTitle, ArticleContent, ArticleComment } = props.article; // Correction ici
 
-    return (
-        <Container className="px-5 my-5">
-            <Row className="gx-5">
-                <div className="col-lg-10 offset-lg-1">
-                    <header className="mb-4">
-                        <h1 className="fw-bolder mb-1">{ArticleTitle}</h1>
-                        <div className="text-muted fst-italic mb-2"></div>
-                    </header>
-                    <figure className="mb-4"><img className="img-fluid rounded" src="https://dummyimage.com/900x400/ced4da/6c757d.jpg" alt="..." /></figure>
-                    <section className="mb-5">
-                        {ArticleContent}
-                    </section>
-                    <h2 className="comment-title">
-                        <span className="comment-count">{ArticleComment}</span> commentaire(s)
-                    </h2>
-                </div>
-            </Row>
-        </Container>
-    );
-};
+//     return (
+//         <Container className="px-5 my-5">
+//             <Row className="gx-5">
+//                 <div className="col-lg-10 offset-lg-1">
+//                     <header className="mb-4">
+//                         <h1 className="fw-bolder mb-1">{ArticleTitle}</h1>
+//                         <div className="text-muted fst-italic mb-2"></div>
+//                     </header>
+//                     <figure className="mb-4"><img className="img-fluid rounded" src="https://dummyimage.com/900x400/ced4da/6c757d.jpg" alt="..." /></figure>
+//                     <section className="mb-5">
+//                         {ArticleContent}
+//                     </section>
+//                     <h2 className="comment-title">
+//                         <span className="comment-count">{ArticleComment}</span> commentaire(s)
+//                     </h2>
+//                 </div>
+//             </Row>
+//         </Container>
+//     );
+// };
