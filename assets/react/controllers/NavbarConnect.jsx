7f2f44eee35a8f1c904cf652db16a7731dbb8392
chloe <michelchloe04@gmail.com>
2024-01-22 10:16:37 +0100

    navbar

--- assets/react/controllers/NavbarConnect.jsx
+++ assets/react/controllers/NavbarConnect.jsx
@@ -18,7 +18,8 @@ function NavbarMenu(props) {
           <Nav className="ms-auto mb-2 mb-lg-0">
             <Nav.Link href={props.LinkHome}>Accueil</Nav.Link>
             <Nav.Link href={props.LinkArticleShow}>Les articles</Nav.Link>
-            <Nav.Link href={props.LinkSignOut}>Se déconnecter</Nav.Link>
+            <Nav.Link href={props.LinkSignIn}>Se connecter</Nav.Link>
+            <Nav.Link href={props.LinkSignUp}>S'inscrire</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
