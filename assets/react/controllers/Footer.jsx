74cfa97dad7ed96935e928b1776f9d207017963b
chloe <michelchloe04@gmail.com>
2024-01-22 10:27:13 +0100

    footer

--- assets/react/controllers/Footer.jsx
+++ assets/react/controllers/Footer.jsx
@@ -6,13 +6,13 @@ export default  function App(props) {
     <footer className="bg-dark py-4 mt-auto sticky-bottom">
         <Container>
           <div className="row align-items-center justify-content-between flex-column flex-sm-row">
-            <div className="col-auto"><div className="small m-0 text-white">Copyright &copy; Your Website 2023</div></div>
+            <div className="col-auto"><div className="small m-0 text-white">Copyright &copy; Cms2023</div></div>
             <div className="col-auto">
-              <a className="link-light small" href="#!">Privacy</a>
+              {/* <a className="link-light small" href="#!">Privacy</a>
               <span className="text-white mx-1">&middot;</span>
               <a className="link-light small" href="#!">Terms</a>
-              <span className="text-white mx-1">&middot;</span>
-              <a className="link-light small" href="#!">Contact</a>
+              <span className="text-white mx-1">&middot;</span> */}
+              <a className="link-light small" href="#!">Contactez-nous</a>
             </div>
           </div>
         </Container>
